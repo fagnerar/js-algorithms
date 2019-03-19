a3f5599eeebbec0e2136a7d2fbf16c95b9b4fc3b
numeros <numeros@tutanota.com>
2019-03-19 15:02:28 -0300

    Factorial with different solutions

--- factorial.js
+++ factorial.js
@@ -1,5 +1,24 @@
-function firstFactorial(num) {
-  return (num > 1) ? num * firstFactorial(num-1) : 1;
+function factorial(num) {
+  return (num > 1) ? num * factorial(num-1) : 1;
 }
 
-console.log(firstFactorial(4)); // 4*3*2*1 = 24
+console.log(factorial(4)); // 4*3*2*1 = 24
+
+function recursiveFactorial(num) {
+  if (num === 1) return 1;
+  else return num * recursiveFactorial(num - 1);
+}
+
+console.log(recursiveFactorial(5)); // 5*4*3*2*1 = 120
+
+function iterativeFactorial(num) {
+  let result = 1;
+
+  for (let i = num; i >= 1; i--) {
+    result *= i;
+  }
+
+  return result;
+}
+
+console.log(iterativeFactorial(6)); // 6*5*4*3*2*1 = 720
